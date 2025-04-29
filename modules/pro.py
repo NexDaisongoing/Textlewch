a16a473ab733c28600e1acbb302c4645e690bf06
NexDaisongoing <125869769+NexDaisongoing@users.noreply.github.com>
2025-04-29 22:20:02 +0530

    Update pro.py

--- modules/pro.py
+++ modules/pro.py
@@ -152,7 +152,7 @@ def pro_feature(bot: Client):
             if test_feature.get(m.chat.id, True):
                 await m.reply_text(f"Here is the FFmpeg command used:\n```
 {cmd}
-```")
+``` ")
 
             await status_message.edit_text(
                 f"✅ **FFmpeg Processing Complete**\n\n"
